521c4fefceaef977bd8a7f42b704fdbe9c7ab600
Sergey <java.courses.2016@gmail.com>
2016-11-21 02:00:04 +0200

    Ex3

--- src/com/company/ex3.java
+++ src/com/company/ex3.java
@@ -1,16 +1,20 @@
+package com.company;
+
 import java.util.Scanner;
 
 public class ex3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: "); //пользователь должен понимать, что от него хочет программа ;)
         int arrSize = sc.nextInt();
         int[] arr = new int[arrSize];
         arr = inputArray(arr);
         outputArray(arr);
-
     }
 
     private static void outputArray(int[] arr) {
+        System.out.print("Ваш массив: ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + "  ");
         }
@@ -19,10 +23,9 @@ public class ex3 {
     public static int[] inputArray(int[] arr) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
+            System.out.print("Введите " + i + "-й элемент массива: ");
             arr[i] = sc.nextInt();
         }
         return arr;
     }
-
-
 }
